d019c28845088f19020a4420a4977dfdafcd63be
Edwin Tandiono <tandiono98@gmail.com>
2018-07-08 21:38:22 +0800

    Added sample platforms

--- Assets/Scripts/MovementControl.cs
+++ Assets/Scripts/MovementControl.cs
@@ -7,6 +7,7 @@ public class MovementControl : MonoBehaviour {
 	[SerializeField] float moveSpeed = 10.0f;
 	[SerializeField] float jumpSpeed = 100.0f;
 	[SerializeField] float nearTreshold = 0.02f;
+	[SerializeField] float tileDistance = 0.5f;
 
 	private bool movementLock = false;
 	private Vector3 target;
@@ -17,9 +18,11 @@ public class MovementControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		Debug.Log (movementLock);
 		if (movementLock && !transform.position.Equals(target)) {
 			transform.position = Vector3.Lerp (transform.position, target, moveSpeed * Time.deltaTime);
 			if (NearTarget ()) {
+				Debug.Log("set to target");
 				transform.position = target;
 				movementLock = false;
 			}
@@ -35,25 +38,25 @@ public class MovementControl : MonoBehaviour {
 			switch (movement) {
 			case "upright":
 				{
-					x = 1;
+					x = 1 + tileDistance;
 					y = 0;
 					break;
 				}
 			case "downright":
 				{
 					x = 0;
-					y = -1;
+					y = -1 - tileDistance;
 					break;
 				}
 			case "downleft":
 				{
 					x = 0;
-					y = 1;
+					y = 1 + tileDistance;
 					break;
 				}
 			case "upleft":
 				{
-					x = -1;
+					x = -1 - tileDistance;
 					y = 0;
 					break;
 				}
@@ -74,7 +77,8 @@ public class MovementControl : MonoBehaviour {
 	}
 
 	bool NearTarget() {
-		return (Vector3.Distance (target, transform.position) < nearTreshold);
+		Debug.Log (transform.position + " " + target + " = " + Vector3.Distance(transform.position, target));
+		return (Vector3.Distance (transform.position, target) < nearTreshold);
 	}
 
 }
